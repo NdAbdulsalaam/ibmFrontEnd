aa0dcc700d67bc09dd16ce553d757b09da79763d
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-12-08 11:29:34 +0100

    handleSubmit

--- react_labs/my-todo/src/App.js
+++ react_labs/my-todo/src/App.js
@@ -4,7 +4,23 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   
   // Add the handlesubmit code here
-  
+  function handlesubmit(e) {
+    e.preventDefault();
+
+    let todo = document.getElementById("todoAdd").value
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+
+    if (newTodo.text.length > 0 ) {
+        setTodos([...todos].concat(newTodo))
+    } else {
+      alert("Enter a valid task")
+    }
+    document.getElementById("todoAdd").value = ""
+  }
   
   // Add the deleteToDo code here
 
@@ -18,10 +34,15 @@ const App = () => {
 return(
 <div className ="App">
 <h1>Todo List</h1>
-<form>
+<form onSubmit={handlesubmit}>
 <input type ="text" align ="right" id= 'todoAdd'/>
 <button type ="submit">Add Todo</button>
 </form>
+  {todos.map((todo) =>
+  <ul className="todo" key={todo.id}>
+    <li className="todo-text">{todo.text}</li>
+  </ul>
+  )}
 </div>
 );
 };
